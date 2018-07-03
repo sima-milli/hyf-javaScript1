f5ee13decea1a6356b406a87c80495294d308a3f
sima <sima.milli@gmail.com>
2018-07-03 13:18:22 +0200

    Declaring variable x

---
+++ week1/script4.js
@@ -0,0 +1,6 @@
+var x;
+console.log('the value of my variable x will be: undefined at first since I have not assigned any thing to it yet');
+console.log(x);
+x = 5; //assigning value to variable x
+console.log('the value of x is now: 5');
+console.log(x);
