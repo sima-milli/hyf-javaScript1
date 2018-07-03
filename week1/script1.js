c8c3fc6cd72465d0b99907b159d272ee240cb66c
sima <sima.milli@gmail.com>
2018-07-03 12:42:03 +0200

    class assignment

---
+++ week1/script1.js
@@ -0,0 +1,4 @@
+var nameArray = ["Androw", "Barbaros", "Donald", "Ernst", "Evgenii", "Gaffari", "Karam", "Marwan", "Said", "Salar", "Sam", "Shadi", "Ward"];
+for (i = 0; i < nameArray.length; i++) {
+    console.log("Hello" + " " + nameArray[i]);
+}
