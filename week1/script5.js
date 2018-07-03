ecffd662a0374e69a586dfb7aa321f12f0f32b55
sima <sima.milli@gmail.com>
2018-07-03 13:38:40 +0200

    Declaring variable y

---
+++ week1/script5.js
@@ -0,0 +1,6 @@
+var y = 'someString';
+console.log('the value of y will be: someString');
+console.log(y);
+y = 'anotherString';
+console.log('the value of y will change to: anotherString');
+console.log(y);
