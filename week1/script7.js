a1eb01a8216811feb55695f6f31e089360326ce7
sima <sima.milli@gmail.com>
2018-07-04 09:37:45 +0200

    Arrays

---
+++ week1/script7.js
@@ -0,0 +1,7 @@
+var emptyArray = [];
+console.log("The value of emptyArray is []");
+console.log(emptyArray);
+var myFavoriteAnimals = ["cat", "dog", "horse", "dolphin"];
+console.log(myFavoriteAnimals);
+myFavoriteAnimals.push("baby big");
+console.log(myFavoriteAnimals);
